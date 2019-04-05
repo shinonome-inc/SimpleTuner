a8fbd2067dfe0216a49cb8184dc8a823d0c2798f
大谷悦志 <otanietsushi@ooyaetsushinoMacBook-puro.local>
2019-04-05 12:40:04 +0900

    プロジェクトとignore作成

---
+++ guitarTuner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guitarTuner
+//
+//  Created by 大谷悦志 on 2019/04/05.
+//  Copyright © 2019 大谷悦志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ guitarTunerTests/guitarTunerTests.swift
@@ -0,0 +1,34 @@
+//
+//  guitarTunerTests.swift
+//  guitarTunerTests
+//
+//  Created by 大谷悦志 on 2019/04/05.
+//  Copyright © 2019 大谷悦志. All rights reserved.
+//
+
+import XCTest
+@testable import guitarTuner
+
+class guitarTunerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
